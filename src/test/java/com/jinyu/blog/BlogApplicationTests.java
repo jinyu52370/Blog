a23fe921cc5faa8ee487de2d54c1e5d5b51d587e
jinyu52370 <jinyu52370@qq.com>
2020-07-02 17:27:32 +0800

    修改测试类

--- src/test/java/com/jinyu/blog/BlogApplicationTests.java
+++ src/test/java/com/jinyu/blog/BlogApplicationTests.java
@@ -1,10 +1,8 @@
 package com.jinyu.blog;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Ignore
 @SpringBootTest
 class BlogApplicationTests {
 
